6b5398c66c1ec075039489f8486cbe30bfb2fc2e
Gustavo Magaña Cardenas <gustavo.magana@sidehouse.mx>
2020-03-04 11:39:41 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Principal from './componentes/Principal';
-import Generico from './componentes/Generico';
 
 function App() {
   return (
     <div className="App">
       <Principal />
-      <Generico />
     </div>
   );
 }
+
+
+export default App
