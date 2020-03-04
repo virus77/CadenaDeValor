d77a03a708e63befe1300bee3a7a5e83f859a541
Consultor Vianse <consultor.vianse@quierocasa.local.com>
2020-03-04 11:52:11 -0600

    ras

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { sp } from '@pnp/sp';
+import { Web } from "@pnp/sp/webs";
+import "@pnp/sp/webs";
+import "@pnp/sp/lists";
+import "@pnp/sp/items";
+import Encabezado from './componentes/Encabezado';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state ={
+    idTerreno: 285,
+    items: []
+  }
+  async componentDidMount(){
+    var currentItem = await sp.web.lists.getByTitle('Terrenos').items.getById(this.state.idTerreno).get();
+    this.setState({items: currentItem});
+  }
+
+  render(){
+    const { idTerreno, items } = this.state;
+    return (
+      <div className="App">
+        <Encabezado terreno = {items} />
+        
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/componentes/Encabezado.js
+++ src/componentes/Encabezado.js
@@ -6,6 +6,8 @@ import user_icon from '../imagenes/user_icon.png';
 import user_icon_clicked from '../imagenes/user_icon_clicked.png';
 import external_icon from '../imagenes/external_icon.png';
 import egupload_icon from '../imagenes/egupload_icon.png';
+import macob from '../imagenes/macob_24.png';
+import macoc from '../imagenes/macoc_16.png';
 import '../estilos/encabezado.css';
 
 const Encabezado = (props) => {
@@ -14,30 +16,33 @@ const Encabezado = (props) => {
     return (
         <div>
             <table className='tabla'>
-                <tr>
-                    <td className='nombreTerreno'><label id='NombreTerreno'><b>{ terreno.NombredelTerreno2 }</b></label></td>
-                    <td className='columna'><img id='FiltroFavoritos' src={favoritos_icon} alt='favoritos_icon' ></img></td>
-                    <td className='columna'><img id='FiltroGantt' src={gantt_icon} alt='gantt_icon' ></img></td>
-                    <td className='columna'><img id='FiltroVerTodo' src={user_icon} alt='user_icon' ></img></td>
-                    <td className='columna'>MACO</td>
-                    <td className='columna'><img id='ToGantt' src={external_icon} alt='external_icon' ></img></td>
-                    <td className='menu'>
-                        <JqxButtonGroup mode='radio'>
-                            <button style={{ padding: '4px 16px' }} id='btn_administracion' value='Administración' />
-                            <button style={{ padding: '4px 16px' }} id='btn_normativo' value='Normativo' />
-                            <button style={{ padding: '4px 16px' }} id='btn_proyectos' value='Proyectos' />
-                            <button style={{ padding: '4px 16px' }} id='btn_eg' value='Estrategia de gestión' />
-                        </JqxButtonGroup>
-                    </td>
-                </tr>
-                <tr>
-                    <td colSpan={7} className='egupload'>
-                        <p>
-                            <img id='CargaEG' src={egupload_icon} alt='egupload_icon' ></img>
-                            E. de G. autorizada
-                        </p>
-                    </td>
-                </tr>
+                <thead>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td className='nombreTerreno'><label id='NombreTerreno'><b>{ terreno.NombredelTerreno2 }</b></label></td>
+                        <td className='columna'><img id='FiltroFavoritos' src={favoritos_icon} alt='favoritos_icon' ></img></td>
+                        <td className='columna'><img id='FiltroGantt' src={gantt_icon} alt='gantt_icon' ></img></td>
+                        <td className='columna'><img id='FiltroVerTodo' src={user_icon} alt='user_icon' ></img></td>
+                        <td className='columna'><img id='MACO' src={macob} alt='macob' ></img></td>
+                        <td className='columna'><img id='ToGantt' src={external_icon} alt='external_icon' ></img></td>
+                        <td className='menu'>
+                            <input type='button' id='btn_administracion' className='boton' value='Administración'></input>
+                            <input type='button' id='btn_normativo' className='boton' value='Normativo'></input>
+                            <input type='button' id='btn_proyectos' className='boton' value='Proyectos'></input>
+                            <input type='button' id='btn_eg' className='boton' value='Estrategia de gestión'></input>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colSpan={7} className='egupload'>
+                            <p>
+                                <img id='CargaEG' src={egupload_icon} alt='egupload_icon' ></img>
+                                E. de G. autorizada
+                            </p>
+                        </td>
+                    </tr>
+                </tbody>
+                
             </table>
         </div>
       );
